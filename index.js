0f987b932ab08771a193525a6692fa28a8e34f3b
Jennifer Wegar <jennyweegar@yahoo.com>
2016-10-28 15:47:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function iterativeLog(array)
+{
+   array.forEach((element, index, array) =>
+   {
+    console.log(`${index}: ${element}`);
+   });
+ }
+
+function iterate (callback)
+{
+  var arr = [1, 2, 3];
+  arr.forEach(callback);
+  return arr
+}
+
+function doToArray(array, callback)
+{
+  return array.forEach(callback);
+}
